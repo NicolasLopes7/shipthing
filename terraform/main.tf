e1225376ff050f05802fedbe441ab4bc2f31fa29
Nicolas Lopes <nicolaslopesaquino@gmail.com>
2024-02-29 13:20:21 -0300

    remove bucket logging

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
 resource "aws_s3_bucket" "shipthing" {
   bucket = "shipthing"
 }
-
-resource "aws_s3_bucket_logging" "shipthing" {
-    bucket = aws_s3_bucket.shipthing.id
-
-    target_bucket = "shipthing-access-logs"
-    target_prefix = aws_s3_bucket.shipthing.id
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
 variable "region" {
   default = "us-east-1"
 }
-
-provider "aws" {
-  region = var.region
-}
-
-
