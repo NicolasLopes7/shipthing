25b0eb5cf9886b0ebfbb15a31b2ff9e26989d292
Emanuel Avila <avilaemanueel@gmail.com>
2024-03-04 22:15:19 -0300

    remove file

--- infra/deployer-service.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find ./deployer-service ./lib ./config -name "*.go" | entr -r sh -c 'echo "Restarted deployer-service" && go run ./deployer-service'
--- infra/uploader-service.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find ./uploader-service ./lib ./config -name "*.go" | entr -r sh -c 'echo "Restarted uploader-service" && go run ./uploader-service'
